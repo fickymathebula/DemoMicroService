83c29e3271ec6f26581f8bd0c37199abe73f711d
fickymathebula <30428905+fickymathebula@users.noreply.github.com>
2018-09-03 21:44:18 +0200

    first commit
    Initial Commit

---
+++ Console_Service_A/Console_Service_A/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Console_Service_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            var name = Console.ReadLine();
+            
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "test",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello my name is, " + name;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "test",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Console_Service_B/Console_Service_B/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Console_Service_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "test",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var myName = message.split(',');
+
+                    Console.WriteLine(" [x] Received {0}", "Hello " + myName[1]+ ", I am your father!");
+                };
+                channel.BasicConsume(queue: "test",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
